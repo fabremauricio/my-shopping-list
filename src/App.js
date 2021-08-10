51e4efd347d513daa0a3151a551bd01034ba7ff3
Mauricio Fabre <fabremauriciomar@gmail.com>
2021-08-09 22:05:11 -0300

    Configure webpack for css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-// import React from "react";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-// import Home from "./components/home";
-// import "./main.css";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Switch>
-//         <Route path="/" component={Home} />
-//       </Switch>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-export default function App() {
-  return <div>This is an example...</div>;
+import Home from "./components/home";
+import "./main.css";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
 }
+
+export default App;
