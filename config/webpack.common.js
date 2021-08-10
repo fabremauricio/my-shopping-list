a28f2a5fdc19a48554e7b11dac069c415daea1af
Mauricio Fabre <fabremauriciomar@gmail.com>
2021-08-09 22:45:39 -0300

    Add css webpack optimization

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -18,11 +18,6 @@ module.exports = {
         test: /.(js|jsx)$/,
         exclude: /node_modules/,
       },
-      {
-        use: ["style-loader", "css-loader"],
-        test: /.(css)$/,
-        exclude: /node_modules/,
-      },
     ],
   },
   resolve: {
